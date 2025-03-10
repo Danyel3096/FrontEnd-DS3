6b936bf060865fdc5227685f685f48314f6f0895
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-03-09 22:38:08 -0500

    Página del carrito de compras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AdminGuard } from './services/admin.guard';
 import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
 import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
 import ProductsListComponent from './pages/products/features/product-list/product-list.component';
+import CartListComponent from './pages/products/cart/cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -13,6 +14,7 @@ export const routes: Routes = [
   { path: 'signup', component: SignupComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'products', component: ProductsListComponent, pathMatch: 'full' },
+  { path: 'cart', component: CartListComponent, pathMatch: 'full' },
   { path: 'admin', component: DashboardComponent, pathMatch: 'full', canActivate: [AdminGuard] },
   { path: 'user-dashboard', component: UserDashboardComponent, pathMatch: 'full', canActivate: [NormalGuard] }
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class', // Asegúrate de que sea 'class'
     content: ["./src/**/*.{html,ts}"],
     theme: {
       extend: {},
