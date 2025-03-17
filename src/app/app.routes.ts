5b27c5261d5fa2a368b7f3400927621be22b4a63
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2025-03-17 14:11:36 -0500

    Agregando paginas y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+// paginas de la aplicacion
+import { HomeComponent } from './pages/home/home.component';
+import { MissingComponent } from './pages/missing/missing.component';
+import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
 
-export const routes: Routes = [];
+// rutas de la aplicacion
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // ruta por defecto
+  { path: 'unauthorized', component: UnauthorizedComponent }, // ruta no autorizada
+  { path: '**', component: MissingComponent }, // ruta no encontrada
+];
