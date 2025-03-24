21326c63f67d90c5627b2d6499d009380be22307
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-03-24 10:39:43 -0500

    New components and pages and updating app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,41 @@
 import { Routes } from '@angular/router';
-import { NormalGuard } from './services/normal.guard';
-import { AdminGuard } from './services/admin.guard';
-import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
-import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+// Ruta por defecto
+import { HomeComponent } from './pages/home/home.component';
+// Rutas de la aplicación
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 import ProductsListComponent from './pages/products/features/product-list/product-list.component';
 import CartListComponent from './pages/products/cart/cart.component';
-
-// paginas de la aplicacion
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { MissingComponent } from './pages/missing/missing.component';
+// Rutas protegidas
+import { NormalGuard } from './services/normal.guard';
+import { AdminGuard } from './services/admin.guard';
+// Rutas del dashboard
+import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { OrdersDashboardComponent } from './pages/orders/orders.component';
+import { ProductsDashboardComponent } from './pages/products/dashboard/dashboard.component';
 import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
+import { MissingComponent } from './pages/missing/missing.component';
 
-// rutas de la aplicacion
+// Arreglo con las rutas de la aplicación
 export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' }, // ruta por defecto
+  // Por defecto
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  // Paginas de la aplicacion
   { path: 'signup', component: SignupComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'settings', component: SettingsComponent, pathMatch: 'full' },
   { path: 'products', component: ProductsListComponent, pathMatch: 'full' },
   { path: 'cart', component: CartListComponent, pathMatch: 'full' },
+  // Páginas protegidas
+  { path: 'user-dashboard', component: UserDashboardComponent, pathMatch: 'full', canActivate: [NormalGuard] },
   { path: 'admin-dashboard', component: DashboardComponent, pathMatch: 'full', canActivate: [AdminGuard] },
+  // Páginas del dashboard
+  { path: 'dashboard-orders', component: OrdersDashboardComponent, pathMatch: 'full', canActivate: [NormalGuard] },
+  { path: 'dashboard-products', component: ProductsDashboardComponent, pathMatch: 'full' },
   //{ path: 'admin-dashboard-product', component: DashboardComponentProduct, pathMatch: 'full', canActivate: [AdminGuard] },
   //{ path: 'admin-dashboard-user', component: DashboardComponentUser, pathMatch: 'full', canActivate: [AdminGuard] },
-  { path: 'user-dashboard', component: UserDashboardComponent, pathMatch: 'full', canActivate: [NormalGuard] },
-  //{ path: 'user-dashboard-order', component: UserDashboardComponentOrder, pathMatch: 'full', canActivate: [NormalGuard] }
   { path: 'unauthorized', component: UnauthorizedComponent }, // ruta no autorizada
   { path: '**', component: MissingComponent }, // ruta no encontrada
 ];
