a0cea9057f4848c96956aab1016bd1e719de0cc1
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-03-28 23:04:12 -0500

    Adding new components for pages

---
+++ src/app/pages/recover-password/user-recover-password.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-recover-password',
+  imports: [],
+  templateUrl: './user-recover-password.component.html',
+  styleUrl: './user-recover-password.component.css'
+})
+export class UserRecoverPasswordComponent {
+
+}
