60f75ad193c4ff7bb9dcd30012efebf09842741a
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-03-28 23:08:58 -0500

    Adding and configuring new services

---
+++ src/app/services/base-http.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class BaseHttpService {
+
+  constructor() { }
+
+  http = inject(HttpClient);
+  apiUrl = environment.API_URL;
+}
