b6569d47a228227b50becd0fb08211e16a45ff02
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-03-28 23:27:14 -0500

    Adding new components for admin dashboard

---
+++ src/app/pages/admin/dashboard/categories-dashboard/categories-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-dashboard',
+  imports: [],
+  templateUrl: './categories-dashboard.component.html',
+  styleUrl: './categories-dashboard.component.css'
+})
+export class CategoriesAdminDashboardComponent {
+
+}
