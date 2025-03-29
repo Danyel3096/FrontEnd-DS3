9ec2bfa3293cf4b9cc6b476d90abe5dee8a102a3
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-03-28 23:12:16 -0500

    Deshabilited SSR temporally and config new app routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
@@ -12,5 +11,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 })
 
 export class AppComponent {
-  title = 'frontend';
+  title = 'Frontend Calidad';
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,32 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'; //OK
+
 // Ruta por defecto
 import { HomeComponent } from './pages/home/home.component';
+
 // Rutas de la aplicación
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
-import { SettingsComponent } from './pages/settings/settings.component';
-import ProductsListComponent from './pages/products/features/product-list/product-list.component';
-import CartListComponent from './pages/products/cart/cart.component';
+import CartListComponent from './pages/cart/cart.component';
+import { UserRecoverPasswordComponent } from './pages/recover-password/user-recover-password.component';
+
 // Rutas protegidas
 import { NormalGuard } from './services/normal.guard';
 import { AdminGuard } from './services/admin.guard';
-// Rutas del dashboard
+
+// Rutas del dashboard de administrador
+import { AdminDashboardComponent } from './pages/admin/admin-dashboard/admin-dashboard.component';
+import { CategoriesAdminDashboardComponent } from './pages/admin/admin-dashboard/categories-dashboard/categories-dashboard.component';
+import { MetricsAdminDashboardComponent } from './pages/admin/admin-dashboard/metrics-dashboard/metrics-dashboard.component';
+import { OrdersAdminDashboardComponent } from './pages/admin/admin-dashboard/orders-dashboard/orders-dashboard.component';
+import { ProductsAdminDashboardComponent } from './pages/admin/admin-dashboard/products-dashboard/products-dashboard.component';
+import { RolesAdminDashboardComponent } from './pages/admin/admin-dashboard/roles-dashboard/roles-dashboard.component';
+import { UsersAdminDashboardComponent } from './pages/admin/admin-dashboard/users-dashboard/users-dashboard.component';
+
+// Rutas del dashboard de usuario
 import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
-import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
-import { OrdersDashboardComponent } from './pages/orders/orders.component';
-import { ProductsDashboardComponent } from './pages/products/dashboard/dashboard.component';
+import { OrdersDashboardComponent } from './pages/user/user-dashboard/orders-dashboard/orders-dashboard.component';
+
+// Otras rutas
 import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
 import { MissingComponent } from './pages/missing/missing.component';
 
@@ -22,20 +34,65 @@ import { MissingComponent } from './pages/missing/missing.component';
 export const routes: Routes = [
   // Por defecto
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'home', redirectTo: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'admin-dashboard', title: 'Admin Dashboard component', component: AdminDashboardComponent, 
+    children: [
+      {
+        path: 'categories', // child admin route path
+        component: CategoriesAdminDashboardComponent, // another child route component that the router renders
+      },
+      {
+        path: 'metrics', // child admin route path
+        component: MetricsAdminDashboardComponent, // another child route component that the router renders
+      },
+      {
+        path: 'products', // child admin route path
+        component: ProductsAdminDashboardComponent, // child route component that the router renders
+      },
+      {
+        path: 'orders', // child admin route path
+        component: OrdersAdminDashboardComponent, // child route component that the router renders
+      },
+      {
+        path: 'roles', // child admin route path
+        component: RolesAdminDashboardComponent, // child route component that the router renders
+      },
+      {
+        path: 'users', // child admin route path
+        component: UsersAdminDashboardComponent, // child route component that the router renders
+      }
+    ] },
   // Paginas de la aplicacion
-  { path: 'signup', component: SignupComponent, pathMatch: 'full' },
-  { path: 'login', component: LoginComponent, pathMatch: 'full' },
-  { path: 'settings', component: SettingsComponent, pathMatch: 'full' },
-  { path: 'products', component: ProductsListComponent, pathMatch: 'full' },
   { path: 'cart', component: CartListComponent, pathMatch: 'full' },
-  // Páginas protegidas
-  { path: 'user-dashboard', component: UserDashboardComponent, pathMatch: 'full', canActivate: [NormalGuard] },
-  { path: 'admin-dashboard', component: DashboardComponent, pathMatch: 'full', canActivate: [AdminGuard] },
-  // Páginas del dashboard
-  { path: 'dashboard-orders', component: OrdersDashboardComponent, pathMatch: 'full', canActivate: [NormalGuard] },
-  { path: 'dashboard-products', component: ProductsDashboardComponent, pathMatch: 'full' },
-  //{ path: 'admin-dashboard-product', component: DashboardComponentProduct, pathMatch: 'full', canActivate: [AdminGuard] },
-  //{ path: 'admin-dashboard-user', component: DashboardComponentUser, pathMatch: 'full', canActivate: [AdminGuard] },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./pages/products/products-list.route'),
+  },
+  {
+    path: 'product',
+    loadChildren: () =>
+      import('./pages/products/product-detail.route'),
+  },
+  { path: 'recover-password', component: UserRecoverPasswordComponent, pathMatch: 'full' },
+  { path: 'signup', component: SignupComponent, pathMatch: 'full' },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./pages/user/user.route'),
+  },
+  // Páginas protegidas en el dashboard
+  { path: 'user-dashboard', title: 'User Dashboard component', component: UserDashboardComponent,
+    children: [
+      {
+        path: 'orders', // child user route path
+        component: OrdersDashboardComponent, // child route component that the router renders
+        pathMatch: 'full', canActivate: [NormalGuard]
+      }
+    ],
+    canActivate: [NormalGuard] },
+  // Otras páginas
   { path: 'unauthorized', component: UnauthorizedComponent }, // ruta no autorizada
   { path: '**', component: MissingComponent }, // ruta no encontrada
 ];
