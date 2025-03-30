fca7fea860357a2af09cd76f2b027b34d845efca
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-03-30 17:47:20 -0500

    Feat: Improving userpage view and config some apis
    Putting and replazing new inputs in the userpage and creating some apis for the dev environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,28 @@
+/*
+  * For easier debugging in development mode, you can import the following file
+  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+  *
+  * This import should be commented out in production mode because it will have a negative impact
+  * on performance if an error is thrown.
+  *
+*/
+/*
+NOTA: Este archivo es para el entorno de desarrollo. Si quieres cambiar la URL de la API, puedes hacerlo aquí.
+Puedes cambiar la URL de la API en el archivo environment.ts para producción cuando tengamos los endpoints del backend.
+*/
 export const environment = {
   production: false,
   API_URL: 'https://fakestoreapi.com',
+  API_URL_PRODUCTO_CREATE: '',
+  API_URL_PRODUCTO_READALL: '',
+  API_URL_PRODUCTO_READBYID: '',
+  API_URL_PRODUCTO_READBYCATEGORY: '',
+  API_URL_PRODUCTO_UPDATE: '',
+  API_URL_PRODUCTO_DELETELOGICALLY: '',
+  API_URL_CATEGORIA_CREATE: '',
+  API_URL_CATEGORIA_READALL: '',
+  API_URL_CATEGORIA_READBYID: '',
+  API_URL_CATEGORIA_UPDATE: '',
+  API_URL_CATEGORIA_DELETELOGICALLY: '',
   //apiUrl: 'http://localhost:8080',  // URL de tu API
 };
