52021892f6d62a706933703f2aa2789c65a54991
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-04-28 21:16:40 -0500

    Feat: Creating and configuring a new component with new SaaS features
    Using dynamic card as template based on fonts and colors customizables

---
+++ src/app/components/dynamic-card/dynamic-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-card',
+  imports: [],
+  templateUrl: './dynamic-card.component.html',
+  styleUrl: './dynamic-card.component.css'
+})
+export class DynamicCardComponent {
+
+}
